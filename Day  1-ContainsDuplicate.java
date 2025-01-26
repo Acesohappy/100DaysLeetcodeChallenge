f9da95025a430ddfeea6096799b79ffaf8fb8cac
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-01-26 18:17:32 +0530

    Create Day  1-ContainsDuplicate.java

---
+++ Day  1-ContainsDuplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> map = new HashSet<>();
+        for (int num : nums) {
+            if (map.contains(num)) {
+                return true;
+            }
+            map.add(num);
+        }
+        return false;
+    }
+}
