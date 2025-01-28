ed1cc626dd677ed91380d7f8a544c462ae8c557a
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-01-28 18:04:28 +0530

    Create Day  3 - Maximum Average SubArray I.java

---
+++ Day  3 - Maximum Average SubArray I.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double avg=0;
+        double sum=0;
+        double max=0;
+        for(int j=0;j<k;j++)
+        {
+            sum+=nums[j];
+        }
+        avg=sum/k;
+        max=avg;
+        for(int i=k;i<nums.length;i++)
+        {
+            sum=sum-(nums[i-k])+nums[i];
+            avg=sum/k;
+            max=Math.max(max, avg);
+        }
+        return max;    
+    }
+}
