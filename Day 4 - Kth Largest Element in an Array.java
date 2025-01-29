436eeea7433639d3f3bb9d9b0261d8a7a8e56235
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-01-29 21:36:01 +0530

    Create Day 4 - Kth Largest Element in an Array.java

---
+++ Day 4 - Kth Largest Element in an Array.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> que=new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++)
+        {
+            que.add(nums[i]);
+        }
+        for(int i=0;i<nums.length-k;i++)
+        {
+            que.remove();
+        }
+        return que.peek();
+    }
+}
