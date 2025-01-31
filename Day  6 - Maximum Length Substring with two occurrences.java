74bd4f921ef453d2c1bdc0a7a8ad0abed4e1573b
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-01-31 20:32:45 +0530

    Create Day  6 - Maximum Length Substring with two occurrences.java

---
+++ Day  6 - Maximum Length Substring with two occurrences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int n = s.length();
+        int left = 0;
+        int max = 0; 
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int right = 0; right < n; right++) {
+            char curr = s.charAt(right);
+            map.put(curr, map.getOrDefault(curr, 0)+1);
+            while (map.get(curr) > 2) {
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
