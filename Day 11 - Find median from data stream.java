ed8b7d997802eaf1ec8d317ca857d44473fce8f1
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-02-05 21:19:17 +0530

    Create Day 11 - Find median from data stream.java

---
+++ Day 11 - Find median from data stream.java
@@ -0,0 +1,32 @@
+class MedianFinder {
+    public PriorityQueue<Integer> minHeap,maxHeap;
+    public MedianFinder() {
+        minHeap=new PriorityQueue<>();
+        maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+    }
+    public void addNum(int num) {
+        if(minHeap.size()==maxHeap.size()){
+            minHeap.add(num);
+            maxHeap.add(minHeap.poll());
+        }
+        else{
+            maxHeap.add(num);
+            minHeap.add(maxHeap.poll());
+        }
+    } 
+    public double findMedian() {
+        if(maxHeap.size()==minHeap.size()){
+            return (maxHeap.peek() + minHeap.peek())/2.0;
+        }
+        else{
+            return maxHeap.peek();
+        }    
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
