fb4df562ffce5ccba2bc563ee70a575915189eb7
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-02-06 20:23:56 +0530

    Create Day 12 - Number of Islands.java

---
+++ Day 12 - Number of Islands.java
@@ -0,0 +1,29 @@
+class Solution {
+    int diff[][]=new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
+    private void dfs(int row,int col,char[][] grid,int i,int j){
+        grid[i][j]='0';
+        for(int k=0;k<4;k++){
+            int adjR=i+diff[k][0];
+            int adjC=j+diff[k][1];
+            if(adjR>=0 && adjR<row && adjC>=0 && adjC<col){
+                if(grid[adjR][adjC]=='1'){
+                    dfs(row,col,grid,adjR,adjC);
+                }
+            }
+        }
+    }
+    public int numIslands(char[][] grid) {
+        int row=grid.length;
+        int col=grid[0].length;
+        int land=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'){
+                    land++;
+                    dfs(row,col,grid,i,j);
+                }
+            }
+        }
+        return land;  
+    }
+}
