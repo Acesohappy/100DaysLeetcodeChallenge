1b18d87d0de9e164644437810c16dee0f047209f
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-02-15 18:51:41 +0530

    Create Day 20 - Consecutive Characters.java

---
+++ Day 20 - Consecutive Characters.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxPower(String s) {
+        int max=0;
+        int curr=0;
+        char ch=s.charAt(0);
+        for(int i=0;i<s.length();i++){
+            if(ch==s.charAt(i)){
+                curr++;
+            }else{
+                ch=s.charAt(i);
+                curr=1;
+            }
+            max=Math.max(max,curr);
+        }
+        return max;
+    }
+}
