0e76ce051db786a4cf78472d685f6e45d8993a57
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-02-26 21:47:40 +0530

    Update Day  2 - Valid Anagram.java

--- Day  2 - Valid Anagram.java
+++ Day  2 - Valid Anagram.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())
         return false;
-
+//comment
         HashMap<Character,Integer> map1=new HashMap<>();
         HashMap<Character,Integer> map2=new HashMap<>();
         for(int i=0;i<s.length();i++)
