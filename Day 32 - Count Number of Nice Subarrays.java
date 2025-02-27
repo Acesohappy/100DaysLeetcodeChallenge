946bb9c9b6b5291b54e775fac803bbea5fce826b
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-02-28 00:13:13 +0530

    Update Day 32 - Count Number of Nice Subarrays.java

--- Day 32 - Count Number of Nice Subarrays.java
+++ Day 32 - Count Number of Nice Subarrays.java
@@ -3,9 +3,15 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
         
-        for(int i=0;i<nums.length;i++)
-        {
-            if(num)
+        int odd = 0;
+        int res= 0;
+        
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                odd++;
+            }
+            res+= map.getOrDefault(odd - k, 0);
+            map.put(odd, map.getOrDefault(odd, 0) + 1);
         }
         
         return res;
