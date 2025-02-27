03dae5d0d1b6454504cd006fd72256d2fcea863d
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-02-28 00:11:44 +0530

    Create Day 32 - Count Number of Nice Subarrays.java

---
+++ Day 32 - Count Number of Nice Subarrays.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(num)
+        }
+        
+        return res;
+    }
+}
