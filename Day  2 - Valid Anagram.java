122694a78cc1567010267e87693fab20dff9cd8c
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-03-10 21:25:14 +0530

    Update Day  2 - Valid Anagram.java

--- Day  2 - Valid Anagram.java
+++ Day  2 - Valid Anagram.java
@@ -7,7 +7,7 @@ class Solution {
         HashMap<Character,Integer> map2=new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
-            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);
+            map1.put(s.charAt(i),map1.getOrDefault(s.charAt(i),0)+1);//comment
             map2.put(t.charAt(i),map2.getOrDefault(t.charAt(i),0)+1);
         }
 
