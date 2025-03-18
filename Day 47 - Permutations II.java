f99b687b36c25a52f354979c6969c544bfa412e4
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-03-18 11:09:55 +0530

    Create Day 47 - Permutations II.java

---
+++ Day 47 - Permutations II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        back(0, nums, new LinkedList<>(), res);
+        return res;
+    }
+    private void back(int i, int[] nums, List<Integer> ans, List<List<Integer>> res) {
+        if (i == nums.length) {
+            if(!res.contains(ans)){
+            res.add(new ArrayList<>(ans));
+            }return;
+        }
+
+        for (int j = 0; j<=i; j++) {
+            ans.add(j, nums[i]);
+            back(i+1, nums, ans, res);
+            ans.remove(j);
+        }
+    }
+}
