881f29d1782229f116995f65cb6d7c0c507d3b90
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-04-28 21:33:26 +0530

    Update Day 8 - Contiguous Array.java

--- Day 8 - Contiguous Array.java
+++ Day 8 - Contiguous Array.java
@@ -2,7 +2,7 @@ class Solution {
     public int findMaxLength(int[] nums) {
         HashMap<Integer,Integer> map=new HashMap<>();
         map.put(0,-1);
-        int curr=0;
+        int curr=0;//comment
         int max=0;
         for(int i=0;i<nums.length;i++)
         {
