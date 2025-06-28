a7b20cf4b8e6e0f3a3a73f5483d685ed44abe1b1
Anubama <135399669+Acesohappy@users.noreply.github.com>
2025-06-28 08:30:10 +0530

    Create Day 76 - Power of Three.java

---
+++ Day 76 - Power of Three.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        for (int i = 0; i < 1291; i++) if (Math.pow(3, i) == n) return true;
+        return false;
+    }
+}
